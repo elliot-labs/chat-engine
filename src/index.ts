dd4c1c9598029f52a8a06ef38dfaa83bcb6308d5
Elliot Huffman <elliot@huffman.me>
2026-07-19 14:05:33 -0400

    Extend Exports
    Types and plugins are now exported at the root level, this improves integration so that the plugins are all available at the root instead of @elliot-labs/chat-engine/bin/src/core/plugins/whatever.js.
    Export types, this is useful if your building your own plugins or trying to properly type your objects being passed into the chat engine's constructor.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
-// Export the ChatEngine class for external usage.
+// #region Core Business Logic
+
+// Export the ChatEngine class, which is the core of the project's logic.
 export { ChatEngine } from './core/ChatEngine.js';
 
-// Export the ChatEnginePlugin class for external usage.
+// Export the ChatEnginePlugin class to allow authors to write their own plugins.
 export { ChatEnginePlugin } from './core/Plugin.js';
+
+// #endregion Core Business Logic
+
+// #region Plugins
+
+// Export a plugin that can perform RAG searches using the cosine similarity algorithm.
+export { pluginSearchRagCosineSimilarity } from './core/plugins/rag.js';
+
+// Export a plugin that can retrieve the current date and time to provide up-to-date context to the LLM.
+export { pluginGetDateTime } from './core/plugins/utility.js';
+
+// #endregion Plugins
+
+// #region Types
+
+// Export the types used in the Chat Engine for type safety and clarity.
+export type { CommonMetadata, ConversationConfig, EmbeddingResult, EmbeddingsConfig } from './types/ChatEngine.js';
+
+// Export the types related to plugins authorship for type safety and clarity.
+export type { ChatEnginePluginConfig, CommonCallbackProps, PluginGroup } from './types/Plugin.js';
+
+// #endregion Types
